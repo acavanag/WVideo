4e4fe0c3c624ba7f1b39e017b7134f1ce03b9441
acavanaghww <acavanagh@weddingwire.com>
2014-03-07 10:03:54 -0500

    Initial Commit

---
+++ WWVideo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WWVideo
+//
+//  Created by Andrew Cavanagh on 3/7/14.
+//  Copyright (c) 2014 WeddingWire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WWAppDelegate class]));
+    }
+}
